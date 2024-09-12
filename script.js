9c25ed2ac585ced8dc0e321f8859b2504c03c7b1
AbaraEmmanuel <141164356+AbaraEmmanuel@users.noreply.github.com>
2024-09-12 23:14:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // GSAP Animations
 document.addEventListener('DOMContentLoaded', () => {
     gsap.from(".profile-pic", { duration: 2, x: -200, opacity: 0, ease: "power2.out" });
-    gsap.from(".hero-text", { duration: 2, x: 200, opacity: 0, delay: 0.5, ease: "power2.out" });
-    gsap.from(".cta-button", { duration: 1, scale: 0.8, opacity: 0, delay: 1.5 });
+    gsap.from(".hero-text", { duration: 2, x: 200, opacity: 0, delay: 1, ease: "power2.out" });
+    gsap.from(".cta-button", { duration: 1, scale: 0.8, opacity: 0, delay: 2 });
     
     // Initialize Lottie Animations
     document.querySelectorAll('.lottie').forEach((element) => {
